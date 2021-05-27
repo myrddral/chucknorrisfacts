8d3ce9f299bde638f49e3162b85699c2973e25ad
Attila Beli <thirdeye0@gmail.com>
2021-05-27 09:46:49 +0200

    refactoring, code cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import { useState } from "react";
+import ChuckNorrisJokes from "./components/ChuckNorrisJokes";
 import "./css/App.css";
-import CategoryList from "./components/CategoryList";
-import FetchedJoke from "./components/FetchedJoke";
 
 function App() {
-  const [chosenCategory, setChosenCategory] = useState(null);
 
   return (
-    <div className="container">
-      <h1 className="header">Chuck Norris jokes</h1>
-      <div className="main">
-        <CategoryList setChosenCategory={setChosenCategory} />
-        {!chosenCategory ? (
-          <h3>Select a category to get a joke!</h3>
-        ) : (
-          <FetchedJoke chosenCategory={chosenCategory} />
-        )}
-      </div>
-    </div>
+    <>
+    <ChuckNorrisJokes />
+    </>
   );
 }
 
---
+++ src/components/ChuckNorrisJokes.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import CategoryList from "./CategoryList";
+import FetchedJoke from "./FetchedJoke";
+
+const ChuckNorrisJokes = () => {
+  const [chosenCategory, setChosenCategory] = useState(null);
+
+  return (
+    <div className="container">
+      <h1 className="header">Chuck Norris jokes</h1>
+      <div className="main">
+        <CategoryList setChosenCategory={setChosenCategory} />
+        {!chosenCategory ? (
+          <h3>Select a category to get a joke!</h3>
+        ) : (
+          <FetchedJoke chosenCategory={chosenCategory} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChuckNorrisJokes;
